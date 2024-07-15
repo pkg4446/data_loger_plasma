0423a5a5f3e4ee8399486585990c18f5fbec0ce2
pkg4446 <pkg4446@gmail.com>
2024-07-15 17:07:22 +0900

    240715

--- routes/device.js
+++ routes/device.js
@@ -1,21 +1,23 @@
 const express   = require('express');
 const router    = express.Router();
 const requestIp = require('request-ip');
+const minute = 1;
+const second = 0;
 
 router.post('/insecticide', async function(req, res) {    
     const IP  = requestIp.getClientIp(req);
     console.log(IP,req.body);
-    res.status(201).json(req.body);
+    res.status(201).send("ack");
 });
 router.post('/refresh', async function(req, res) {    
     const IP  = requestIp.getClientIp(req);
     console.log(IP,req.body);
-    res.status(201).json(req.body);
+    res.status(201).send(`min:${minute},sec:${second}`);
 });
 router.post('/runtime', async function(req, res) {    
     const IP  = requestIp.getClientIp(req);
     console.log(IP,req.body);
-    res.status(201).json(req.body);
+    res.status(201).send("ack");
 });
 
 module.exports = router;

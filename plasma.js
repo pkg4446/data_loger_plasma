73b2f46d0b0a7e7a345dc5e7552d117341113f52
pkg4446 <pkg4446@gmail.com>
2024-07-19 09:53:49 +0900

    240719

--- plasma.js
+++ plasma.js
@@ -2,7 +2,7 @@ const express     = require('express');
 const indexRouter = require('./routes');
 
 const app   = express();
-const port  = 3000;
+const port  = 3002;
 
 app.use(express.json());
 app.use('/', indexRouter);
